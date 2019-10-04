7deb348a53cd5c054ce8913a851288387b78fc80
Evgeny Chumak <chumak.e.ol@sberbank.ru>
2019-10-04 11:54:04 +0300

    initial commit

---
+++ app/src/main/java/com/example/learningprogram/models/DisplayMode.java
@@ -0,0 +1,37 @@
+package com.example.learningprogram.models;
+
+import androidx.annotation.StringRes;
+
+import com.example.learningprogram.R;
+
+/**
+ * Режимы отображения списка лекций
+ *
+ * @author Evgeny Chumak
+ **/
+public enum DisplayMode {
+    /**
+     * Без группировки - элементы отображаются один за одним
+     */
+    UNGROUPED(R.string.ungrouped),
+    /**
+     * С группировкой по неделям - перед лекциями соответствующей недели появляется
+     * ещё 1 элемент списка, в котором указан номер недели
+     */
+    GROUP_BY_WEEK(R.string.group_by_week);
+
+    @StringRes
+    private final int mTitleStringResourceId;
+
+    DisplayMode(@StringRes int titleStringResourceId) {
+        mTitleStringResourceId = titleStringResourceId;
+    }
+
+    /**
+     * Каким строковым ресурсом можно отобразить пользователю соответствующую константу {@link DisplayMode}
+     */
+    @StringRes
+    public int getTitleStringResourceId() {
+        return mTitleStringResourceId;
+    }
+}
---
+++ app/src/main/java/com/example/learningprogram/models/Lecture.java
@@ -0,0 +1,74 @@
+package com.example.learningprogram.models;
+
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
+
+/**
+ * Модель лекции
+ *
+ * @author Evgeny Chumak
+ **/
+public class Lecture {
+    private static final int LECTURES_PER_WEEK = 3;
+
+    private final int mNumber;
+    private final String mDate;
+    private final String mTheme;
+    private final String mLector;
+    private final int weekIndex;
+
+    public Lecture(
+            int number,
+            @NonNull String date,
+            @NonNull String theme,
+            @NonNull String lector
+    ) {
+        mNumber = number;
+        mDate = date;
+        mTheme = theme;
+        mLector = lector;
+        weekIndex = (mNumber - 1) / LECTURES_PER_WEEK;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lecture lecture = (Lecture) o;
+        return mNumber == lecture.mNumber &&
+                weekIndex == lecture.weekIndex &&
+                Objects.equals(mDate, lecture.mDate) &&
+                Objects.equals(mTheme, lecture.mTheme) &&
+                Objects.equals(mLector, lecture.mLector);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mNumber, mDate, mTheme, mLector, weekIndex);
+    }
+
+    public int getNumber() {
+        return mNumber;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public String getTheme() {
+        return mTheme;
+    }
+
+    public String getLector() {
+        return mLector;
+    }
+
+    public int getWeekIndex() {
+        return weekIndex;
+    }
+}
